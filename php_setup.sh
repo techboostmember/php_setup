e78af199b898fe27bc3918da02735cb6f0b4305f
techboost <tb_support@b-engineer.com>
2022-01-12 20:40:38 +0900

    test

---
+++ php_setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+sudo yum update -y
+# mysql
+sudo service mariadb stop
+sudo yum -y erase mariadb-config mariadb-common mariadb-libs mariadb
+sudo yum localinstall https://dev.mysql.com/get/mysql80-community-release-el7-1.noarch.rpm -y
+sudo yum-config-manager --disable mysql80-community
+sudo yum-config-manager --enable mysql57-community
+sudo yum install mysql-community-server -y
+sudo systemctl start mysqld.service 
+sudo systemctl enable mysqld.service
+# php
+sudo yum remove -y php php-*
+sudo amazon-linux-extras disable lamp-mariadb10.2-php7.2
+sudo amazon-linux-extras install php8.0 -y
+sudo amazon-linux-extras enable php8.0
+sudo yum install php-mbstring php-pdo php-mysqlnd php-bcmath php-xml -y
+sudo rm -f /etc/php.d/30-xdebug.ini
+# composer
+curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/bin/composer
+# heroku
+curl -OL https://cli-assets.heroku.com/heroku-linux-x64.tar.gz
+tar zxf heroku-linux-x64.tar.gz && rm -f heroku-linux-x64.tar.gz
+sudo mv heroku /usr/local
+echo 'export PATH=/usr/local/heroku/bin:$PATH' >> $HOME/.bash_profile
+source $HOME/.bash_profile
